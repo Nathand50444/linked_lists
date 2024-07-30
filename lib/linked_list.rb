b080c80bf1327ff5e882d428169a2fedc083b57c
NathanD <nathand50444@gmail.com>
2024-07-30 21:23:01 +0100

    Lists can now be built with append and to_s

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,16 +1,22 @@
 class LinkedList
   def initialize
-    @list = [nil]
+    @list = ["nil"]
     to_s
   end
 
   def append(value)
-    @list.append(value)
+    if @list.length == 1
+      @list.prepend(value)
+    else
+      @list.insert(-2, value)
+    end
+    puts @list
+    to_s
   end
 
   def to_s
     puts "What should we add to our Linked List?"
-    input = "( #{gets} ) ->"
+    input = "( #{gets.chomp} ) ->"
     append(input)
   end
 end
